7f1c5a8b557f7f38e3ca36f317adcd479dba39bf
Sujal Sinha <sujalsinha2243@gmail.com>
2023-06-25 13:37:08 +0530

    small change

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,7 +25,7 @@ export default function Form({ onAddItem }) {
 
 	return (
 		<form className="add-form" onSubmit={handleSubmit}>
-			<h3>What do you need for your 😍 trip?</h3>
+			<h3>What do you need for your trip 😍?</h3>
 			<select
 				value={quantity}
 				onChange={(e) => setQuantity(Number(e.target.value))}
